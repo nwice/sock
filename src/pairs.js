467c8eed813fcd9f8e6cf186cf41de8f5f90b793
Kevin McIntyre <kebin70@gmail.com>
2021-03-16 23:19:31 +0000

    l/g

--- src/pairs.js
+++ src/pairs.js
@@ -13,7 +13,7 @@ let info = { pairs: [
     { pair: 'avax_png', tvl: 0, account: '0x1eC206a9dD85625E1940cD2B0c8e14a894D2e9aC' },
     { pair: 'avax_snob', tvl: 0, account: '0xB12531a2d758c7a8BF09f44FC88E646E1BF9D375' },
     { pair: 'avax_sushi', tvl: 0, account: '0x14ec55f8B4642111A5aF4f5ddc56B7bE867eB6cC' }    
-], tvl: 0};
+], tvl: 0}
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -23,18 +23,18 @@ let info = { pairs: [
     const page = await browser.newPage()
 
     for (let x = 0; x < info.pairs.length; x++) {
-        let pair = info.pairs[x];
+        let pair = info.pairs[x]
         await page.goto('https://info.pangolin.exchange/#/account/' + pair.account)
         await delay(10000);
         const liquidity = await page.evaluate(() => {
             let l = 0;
             [].forEach.call(document.querySelectorAll('div'), function (div) {
                 if (div.innerHTML === 'Liquidity (Including Fees)') {
-                    l = div.parentNode.nextElementSibling.firstElementChild.innerHTML;
+                    l = div.parentNode.nextElementSibling.firstElementChild.innerHTML
                 }
-            });
+            })
             return l
-        });
+        })
         try {
             pair.tvl = parseFloat(liquidity.replace(/[^0-9.-]+/g, ''))
             if ( isNaN(pair.tvl) ) {
@@ -47,9 +47,10 @@ let info = { pairs: [
             x = x - 1
             console.log('again:', err)
         }
-    };
+    }
     await browser.close()
+    info.date = new Date().toISOString()
     info.tvl = info.pairs.map(p => { return p.tvl }).reduce((a, b) => a + b)
     console.log('total:', '$' + info.tvl.toFixed(2))
-    fs.writeFileSync('data/pairs.json', JSON.stringify(info, null, 2));
+    fs.writeFileSync('data/pairs.json', JSON.stringify(info, null, 2))
 })()
