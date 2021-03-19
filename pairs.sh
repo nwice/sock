ee32e5f614c3fb211a401899699b0322f49f0fd3
Kevin McIntyre <kebin70@gmail.com>
2021-03-19 19:37:59 +0000

    l/g

--- pairs.sh
+++
@@ -1,6 +0,0 @@
-node ./src/pairs.js
-aws s3 cp ./public/pairs.html s3://beta.scewpt.com/pairs --acl public-read --content-type text/html
-aws s3 cp ./public/pairs.json s3://beta.scewpt.com/pairs.json --acl public-read
-aws cloudfront create-invalidation --distribution-id E11ZCATVMKXF7D --paths "/pairs.json" "/pairs"
-#aws cloudfront create-invalidation --distribution-id E11ZCATVMKXF7D --paths "/pairs.json" "/pairs" "/assest/*"
-#aws s3 sync public/assets s3://beta.scewpt.com/assets --acl public-read
