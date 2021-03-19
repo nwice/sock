c98918898425a07c01f61c8a88d4dfd2e0d3021a
Kevin McIntyre <kebin70@gmail.com>
2021-03-19 22:14:20 +0000

    l/g

--- supply.sh
+++ supply.sh
@@ -1,3 +1,4 @@
 node ./src/supply.js
-aws s3 cp ./public/supply.json s3://beta.scewpt.com/supply --acl public-read --content-type application/json
-aws cloudfront create-invalidation --distribution-id E11ZCATVMKXF7D --paths "/supply"
+aws s3 cp ./public/supply.html s3://beta.scewpt.com/supply --acl public-read --content-type text/html
+aws s3 cp ./public/supply.json s3://beta.scewpt.com/supply.json --acl public-read --content-type application/json
+aws cloudfront create-invalidation --distribution-id E11ZCATVMKXF7D --paths "/supply" "/supply.json"
