4fca74eb1658f920d324dc42912077690149ff37
Kevin McIntyre <kebin70@gmail.com>
2021-03-25 17:20:38 +0000

    l/g

--- src/price.js
+++ src/price.js
@@ -1,47 +1,57 @@
 const puppeteer = require('puppeteer');
 const WebSocket = require('ws');
 
-console.log('price service')
+var AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-east-1' });
+
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+let table = 'price'
+
+let long_dive = true;
+
+console.log('price service:', long_dive)
 
 let specific_dex = {
     value: 0, 
     token: 'PNG',
     info: {
         tokens: 'https://info.pangolin.exchange/#/tokens',
-        trigger_div: 'WAVAX'
+        match_inner_html: 'WAVAX'
     }
 }
 
 const server = new WebSocket.Server({
-  port: 8081,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      // See zlib defaults.
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
-    // Other options settable:
-    clientNoContextTakeover: true, // Defaults to negotiated value.
-    serverNoContextTakeover: true, // Defaults to negotiated value.
-    serverMaxWindowBits: 10, // Defaults to negotiated value.
-    // Below options specified as default values.
-    concurrencyLimit: 10, // Limits zlib concurrency for perf.
-    threshold: 1024 // Size (in bytes) below which messages
-    // should not be compressed.
-  }
+  port: 8081
 });
 
 server.on('connection', function connection(ws) {
     ws.on('message', (message) => {
-        console.log('message:', message);
-    });      
+        try {
+            let msg = JSON.parse(message);
+            //console.log('price change:', msg)
+            ddb.putItem({
+                TableName: 'price',
+                Item: AWS.DynamoDB.Converter.marshall(msg)
+            }, function (err, data) {
+                if (err) {
+                   console.log('put error:');
+                } else {
+                    console.log('put success:', data);
+                }
+            });             
+        } catch (err) {
+            console.log('text:', message)
+        }
+    });
+    ws.on('close', (event) => {
+        console.log('close:', event);
+    });
+    ws.on('open', (event) => {
+        console.log('open:', event);
+    });             
 });
-
-let forever_after = true;
 
 (async () => {
 
@@ -49,51 +59,124 @@ let forever_after = true;
     const height = 1080
     const browser = await puppeteer.launch({
         headless: true,
+        userDataDir: '/tmp/user-data-dir-' + Math.floor((Math.random() * 10000000000) + 1),
         args: ['--no-sandbox', '--disable-setuid-sandbox', `--window-size=${ width },${ height }`],
     })
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage()
-    await page.setViewport({ width: 1920, height: 1080});
-    await page.goto(specific_dex.info.tokens, { waitUntil: 'domcontentloaded' });
-    let div_altitude = 0;
-    let level_flight = 500;
-    while(div_altitude < level_flight) {
-        await new Promise(function (resolve) {
-            setTimeout(resolve, 3000)
-        });
-        try {
-            console.log('pre-eval div altitude:', div_altitude)
-            div_altitude = await page.evaluate( (dex) => {
-                let divs = document.querySelectorAll('div');
-                if ( divs.length < 500 ) {
-                    return divs.length
-                } else {
-                    let token_divs = [];
-                    for (var x = 0; x < divs.length; x++) {
-                        if ( divs[x].innerHTML === dex.info.trigger_div ) {
-                            token_divs = [...divs[x].parentNode.parentNode.parentNode.parentNode.querySelectorAll(':scope > div:not(:first-of-type)')];
-                            break;
-                        }
-                    }
-                    if ( token_divs.length == 0 ) {
-                        return divs.length
-                    }
-                    return new Promise ((resolve, reject) => {
-                        const client = new WebSocket('ws://localhost:8081');
-                        client.addEventListener('open', function (event) {
-                            client.send(`in tower with ${token_divs.length} tokens`);
-                        });
-                    });                    
-                };
-                return divs.length;
-            }, specific_dex);
-        } catch (err) {
-            console.log('error:', err)    
+    page.on('request', request => {
+        if ( 
+            request.url().indexOf('raw.githubusercontent.com') < 0 && 
+            request.url().indexOf('graph-node.avax.network') < 0 &&
+            request.url().indexOf('unpkg.com') < 0 &&
+            request.url().indexOf('data:image/png;base64') < 0 ) {
+            console.log('request:', request.url());
         }
+    });
+    await page.setViewport({ width: 1920, height: 1080});
+    const session = await page.target().createCDPSession();
+    await session.send("Page.enable");
+    await session.send("Page.setWebLifecycleState", { state: "active" });
+
+    await page.goto(specific_dex.info.tokens, { waitUntil: 'domcontentloaded' });
+    console.log('diver - step off')
+    await page.evaluate((dex) => {
+
+        document.dex = dex;
+
+        return new Promise ((resolve, reject) => {
+            const client = new WebSocket('ws://localhost:8081');
+            
+            document.addEventListener('price', function (e) {
+                //client.send(JSON.stringify(e.detail))
+
+                let rank = e.detail.querySelector(':scope > div > div > div').innerHTML
+                let hash = e.detail.querySelector(':scope > div > div > a').href.split('/').pop();
+                let token = e.detail.querySelector(':scope > div:nth-of-type(2) > div').innerHTML
+                let liquidity = e.detail.querySelector(':scope > div:nth-of-type(3)').innerHTML.replace(/[^\d.-]/g, '')
+                let volume = e.detail.querySelector(':scope > div:nth-of-type(4)').innerHTML.replace(/[^\d.-]/g, '')
+                let price = e.detail.querySelector(':scope > div:nth-of-type(5)').innerHTML.replace(/[^\d.-]/g, '')
+                let change = e.detail.querySelector(':scope > div:nth-of-type(6) > div').innerHTML
+                let obj = { rank, hash, token, liquidity, volume, price, change, timestamp: new Date().getTime() }
+
+                if ( parseInt(rank) < 12) {
+                    client.send('token:' + JSON.stringify(obj));
+                }
+                //client.send(JSON.stringify(obj))
+
+            }, false);
+
+            document.addEventListener('chat', function (e) {
+                client.send(e.detail)
+            }, false);
+
+            document.addEventListener('watchtoken', function (e) {
+                const observer = new MutationObserver((mutationsList, observer) => {
+                    client.send('mutation change')
+                });
+                observer.observe(e.detail, { attributes: true, childList: true, subtree: true });
+            }, false);
+
+            document.addEventListener('foundking', (e) => {
+                // need to iterate through div
+                // mutation observer not working...yet.
+                // 
+                let king = e.detail;
+                let throne = king.parentNode.parentNode;
+                let tokens = [...throne.parentNode.parentNode.querySelectorAll(':scope > div > div:nth-of-type(1)')];
+                tokens.forEach(t=> {
+                    document.dispatchEvent(new CustomEvent('price', {
+                        detail: t
+                    }));
+                })
+                setInterval( () => { 
+                    // may comfirm pair liquidity -- maybe that trips price updates
+
+                }, 30000);
+
+            });
+
+            client.addEventListener('open', function (event) {
+                client.send(`open ${dex.token} to ${document}`);
+                resolve()
+            });
+
+            client.addEventListener('close', function (event) {
+                client.send(`close ${dex.token} to ${document}`);
+                reject()
+            });             
+        });     
+    }, specific_dex);
+    let submerged = 500;
+    
+    await page.waitForFunction(`document.querySelectorAll('div').length > ${submerged}`);
+
+    let submerged_divs = await page.evaluate(() => {
+        return Promise.resolve(document.querySelectorAll('div').length)
+    });
+    console.log('submerged divs:', submerged_divs)  
+    if (long_dive) {
+        console.log('long dive good luck');
+        await page.evaluate(() => {
+            return new Promise ((resolve, reject) => {
+                let all_divs = document.querySelectorAll('div');
+                document.dispatchEvent(new CustomEvent('chat', {
+                    detail: 'landed:' + all_divs.length
+                }));
+                let king_div = null;
+                [].forEach.call(all_divs, (div) => {
+                    if ( div.innerHTML === document.dex.info.match_inner_html ) {
+                        king_div = div                        
+                    }
+                });
+                if (king_div != null) {
+                    document.dispatchEvent(new CustomEvent('foundking', {
+                        detail: king_div
+                    }));
+                }
+            });
+        });
     }
-    if (forever_after) {
-        console.log('forever after');
-        await new Promise ((resolve, reject) => {});
-    }
+    console.log('before close - assuming task complete');
     await browser.close();
 })()
