76e689c76150d81afd0c67b862837a16737d1881
Kevin McIntyre <kebin70@gmail.com>
2021-03-27 07:57:09 +0000

    l/g

--- assets.sh
+++ assets.sh
@@ -1,3 +1,3 @@
 aws s3 sync public/assets s3://beta.scewpt.com/assets --acl public-read
-aws s3 sync public/common.js s3://beta.scewpt.com/common.js --acl public-read
+aws s3 cp public/common.js s3://beta.scewpt.com/common.js --acl public-read
 aws cloudfront create-invalidation --distribution-id E11ZCATVMKXF7D --paths "assets" "common.js"
