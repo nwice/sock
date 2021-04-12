d7026ccaae4c1c523d9a8d8b479a5d491aa62f59
Kevin McIntyre <kebin70@gmail.com>
2021-04-12 19:46:49 +0000

    l/g

--- bin/supply.sh
+++ bin/supply.sh
@@ -1,4 +1,5 @@
 cd /home/ubuntu/sock
+
 node ./src/supply.js
 
 aws cloudfront create-invalidation --distribution-id E1JZNMX0HG4856 --paths "/supply/snob.json" "/snob/circulating"
--- bin/tvl.sh
+++ bin/tvl.sh
@@ -1,3 +1,5 @@
+cd /home/ubuntu/sock/
+
 node ./src/tvl.js
 
 aws cloudfront create-invalidation --distribution-id E1JZNMX0HG4856 --paths "/tvl" "/tvl/snob.json" "/snob/tvl"
