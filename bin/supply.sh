223465e51bbb0214d971d6eca68e47d6ece62bee
Kevin McIntyre <kebin70@gmail.com>
2021-04-26 00:21:15 +0000

    l/g

--- bin/supply.sh
+++ bin/supply.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/sock
 
-node ./src/supply.js
+node ./src/cli.js supply
 
 aws cloudfront create-invalidation --distribution-id E1JZNMX0HG4856 --paths "/snob/circulating" "/supply/snob.json"
